15c52a8c8cdc40bc984a4fd63527ba3b1a7b8be7
anushka chauhan <anushkachauhanannu@gmail.com>
2026-02-24 00:00:13 +0530

    Updated File Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSideBar from "./components/ProjectsSideBar";
-
+import {useState} from 'react';
 function App() {
+   const [ProjectsState,setProjectsState] = useState({
+    //undefined means doing nothing
+    selectedProjectId: undefined,
+    project: []
+   });
+   function handleStartAddProject(){
+    setProjectsState(prevState =>{
+      return {
+        //null means the signal to us that we are adding a new project 
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+   }
+   let content;
+   if(ProjectsState.selectedProjectId === null){
+    content =<NewProject/>
+   }else if(ProjectsState.selectedProjectId === undefined){
+    content= <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+   }
   return (
     <main className="h-screen my-8 flex gap-8 ">
-     <ProjectSideBar/>
-     <NoProjectSelected/>
+     <ProjectSideBar onStartAddPRoject={handleStartAddProject}/>
+     <NoProjectSelected onStartAddPRoject={handleStartAddProject}/>
+     {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,13 +1,13 @@
 import noProjectImage from '../assets/no-projects.png'
 import Button from './Button.jsx';
-export default function NoProjectSelected(){
+export default function NoProjectSelected({onStartAddProject}){
   return(
 <div className="mt-24 text-center w-2/3 ">
   <img src={noProjectImage} alt="An empty tassk list" className="w-16 h-16 object-contain mx-auto "/>
   <h2 className ="text-xl font-bold text-stone-500 my-4 ">No Project Selected</h2>
   <p className="test-stone-400 mb-4 ">Select a project or get started with the new One</p>
   <p className="mt-8">
-    <Button>Create New Project</Button>
+    <Button onClick = {onStartAddProject}>Create New Project</Button>
   </p>
 </div>
   );
